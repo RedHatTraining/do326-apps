d18867d80a8bd04e14bc9c74ff82a8ac62febe8a
Benjamin Chardi <bchardim@redhat.com>
2021-05-12 11:38:40 +0200

    CH07 - Downtime Network-  node-app-cm (#1)
    * add node-app-cm app
    
    * fixed git refs
    
    * add node-app.yaml

---
+++ node-app-cm/example1/delete-all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Deleting the projects ...."
+oc delete project/configmap-example
---
+++ node-app-cm/example2/load-all.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# You need 2 users (dev and test)
+
+oc new-project node-app-dev
+oc create -f configmap-dev.json
+oc create -f node-app-deployment.json
+oc create -f node-app-build.json
+
+oc new-project node-app-test
+oc create -f configmap-test.json
+oc create -f node-app-deployment.json
+
+
+oc create -f roles.json
+oc adm policy add-role-to-user image-tagger dev -n demo-app-test 
+oc adm policy add-role-to-user image-puller system:serviceaccount:node-app-test:deployer -n demo-app-dev
+
+
+# Login in back as developer
+echo ""
+echo ""
+echo "You're now user: dev"
+
+echo ""
+echo "Building the app as developer:"
+echo ""
+echo "  oc start-build node-app -n node-app-dev --from-dir=.. --follow  "
+echo ""
+echo ""
+echo ""
+echo "If you want to promote the application, you can:"
+echo ""
+echo "   oc tag node-app-dev/node-app:latest node-app-test/node-app:latest   "
+echo ""
+echo ""
